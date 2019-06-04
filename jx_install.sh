53a32d5aa60b1a8071e4bc2da9e9fed314ae1b6f
yrj2011 <470018286@qq.com>
2019-06-04 19:51:18 +0000

    修改脚本

--- jx_install.sh
+++ jx_install.sh
@@ -23,7 +23,7 @@ kubectl apply -f  pv-7.yaml
 kubectl apply -f  pv.yaml
 unset http_proxy
 unset https_proxy
-PATH=/opt/cicd/sh/git:$PATH
+PATH=/opt/cicd/sh/git/git:$PATH
 myjx install --provider=kubernetes  --on-premise --default-admin-password='123456' \
 --cloud-environment-repo http://192.168.1.228:1080/jenkins-x/cloud-environments.git \
 --versions-repo http://192.168.1.228:1080/jenkins-x/jenkins-x-versions.git \
